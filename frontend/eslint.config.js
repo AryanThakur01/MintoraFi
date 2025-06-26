655cb1f25cd48a67fbc14dd4e32ac94a383899e6
Aryan Thakur <aryan197297@gmail.com>
2025-06-26 17:49:13 +0530

    feat: add vite tailwind shadcn for development

---
+++ frontend/eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js'
+import pluginPrettier from 'eslint-plugin-prettier'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+      'prettier/prettier': 'error', // ✅ Enforce Prettier formatting
+    },
+    plugins: {
+      prettier: pluginPrettier,
+    },
+  },
+])
