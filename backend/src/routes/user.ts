676bdc989574f71a02f335b02338c0157fea88c2
Aryan Thakur <aryan197297@gmail.com>
2025-07-03 00:59:48 +0530

    feat: add nft token info fetching api

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -90,25 +90,4 @@ router.get('/account', async (req, res) => {
   }
 })
 
-router.get('/nft/:tokenId', async (req, res) => {
-  try {
-    if (!req.user) {
-      sendResponse(res, ResponseStatus.UNAUTHORIZED, 'Session expired or user not authenticated')
-      return
-    }
-    const { tokenId } = req.params
-    const hederaAccount = await prisma.hederaAccount.findUnique({ where: { userId: req.user.id } })
-    if (!hederaAccount) {
-      sendResponse(res, ResponseStatus.NOT_FOUND, 'Hedera account not found for user')
-      return
-    }
-    const accountService = new HederaAccountService()
-    const nftInfo = await accountService.getNftInfo(tokenId)
-    sendResponse(res, ResponseStatus.SUCCESS, 'NFT information retrieved successfully', nftInfo)
-  } catch (error) {
-    if (error instanceof Error) sendResponse(res, ResponseStatus.BAD_REQUEST, error.message)
-    else sendResponse(res, ResponseStatus.INTERNAL_SERVER_ERROR, ResponseMessage.INTERNAL_SERVER_ERROR)
-  }
-})
-
 export default router
